fbb841a53d660b8dd4eb81ef8f30a89f228a5032
Yasuhiro Usutani <y_usutani@mac.com>
2020-05-10 15:00:50 +0900

    Add speak IF to RoomChannel

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,12 +1,15 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    # stream_from 'room_channel'
+    stream_from 'room:message'
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
+  def speak(data)
+    # ActionCable.server.broadcast 'room_channel', message: data['message']
+    RoomChannel.broadcast_to('message', data)
   end
 end
